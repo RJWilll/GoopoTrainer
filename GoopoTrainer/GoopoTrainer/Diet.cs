63e2cac8c57d9e76fafee36ce846c83840f73169
Reed Williams <140353986+RJWilll@users.noreply.github.com>
2026-07-21 02:52:29 -0700

    Basic Class Architecture

---
+++ GoopoTrainer/GoopoTrainer/Diet.cs
@@ -0,0 +1,14 @@
+﻿using Android.Telephony;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoopoTrainer
+{
+    public class Diet
+    {
+        public int strengthGain;
+        public int sizeGain;
+        public int frequencyMinutes;
+    }
+}
